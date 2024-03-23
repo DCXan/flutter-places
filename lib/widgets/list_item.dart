ab75c72835e6ff612101332946dd272e463e2a07
David Chen <chen.david.1016@gmail.com>
2024-03-22 23:29:25 -0500

    added place details screen

--- lib/widgets/list_item.dart
+++ lib/widgets/list_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_places/models/place.dart';
+import 'package:flutter_places/screens/place_detail.dart';
 
 class ListItem extends StatelessWidget {
   const ListItem({super.key, required this.place});
@@ -9,6 +10,13 @@ class ListItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InkWell(
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: ((context) => PlaceDetailScreen(place: place)),
+          ),
+        );
+      },
       child: ListTile(
         title: Text(
           place.name,
